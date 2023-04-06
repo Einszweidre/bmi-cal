2c8546ecebc9f6500d2da416aad7219295c74e60
Blestro Bellarmino <blestrohuta@gmail.com>
2023-04-06 15:05:44 +0700

    rekomendasi weight status

--- recommendation.js
+++ recommendation.js
@@ -1,22 +1,3 @@
-function calculateBMI() {
-    let height = parseFloat(document.getElementById("height").value);
-    let weight = parseFloat(document.getElementById("weight").value);
-
-    let bmi = weight / (height ** 2);
-
-    document.getElementById("result").innerHTML = "Your BMI is: " + bmi.toFixed(2);
-
-    if (bmi < 18.5) {
-        document.getElementById("category").innerHTML = "You are underweight.";
-    } else if (bmi >= 18.5 && bmi < 25) {
-        document.getElementById("category").innerHTML = "You are of normal weight.";
-    } else if (bmi >= 25 && bmi < 30) {
-        document.getElementById("category").innerHTML = "You are overweight.";
-    } else {
-        document.getElementById("category").innerHTML = "You are obese.";
-    }
-}
-
 let recommendations = [
     {
         category: "Underweight",
