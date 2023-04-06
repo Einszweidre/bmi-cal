dd5824b372185fbf260da799e07bfc8f64a771e2
Blestro Bellarmino <blestrohuta@gmail.com>
2023-04-06 15:20:17 +0700

    recommendation status weight

--- index.js
+++ index.js
@@ -16,3 +16,38 @@ function calculateBMI() {
         document.getElementById("category").innerHTML = "You are obese.";
     }
 }
+
+let recommendations = [
+    {
+        category: "Underweight",
+        tips: [
+            "Kenaikan berat badan sehat dengan meningkatkan asupan kalori secara bertahap dan seimbang.",
+            "Latihan kekuatan untuk membangun otot dan meningkatkan kekuatan tubuh.",
+            "Konsumsi makanan yang kaya nutrisi seperti biji-bijian, protein, lemak sehat dan buah-buahan dan sayuran yang memiliki kandungan nutrisi yang tinggi."
+        ]
+    },
+    {
+        category: "Normal",
+        tips: [
+            "Berolahraga teratur dengan aktivitas aerobik seperti jogging, bersepeda, berenang atau yoga.",
+            "Makan seimbang dengan mengonsumsi makanan yang kaya nutrisi dan tidak berlebihan dalam porsi.",
+            "Tidur cukup untuk memulihkan energi dan memperbaiki sistem kekebalan tubuh."
+        ]
+    },
+    {
+        category: "Overweight",
+        tips: [
+            "Berolahraga secara teratur dengan aktivitas aerobik dan kekuatan untuk membakar kalori dan membangun otot.",
+            "Konsumsi makanan yang seimbang dan terkontrol dalam porsi dan kalori.",
+            "Pilih makanan dengan kandungan rendah kalori dan tinggi nutrisi seperti buah-buahan, sayuran, protein rendah lemak, dan biji-bijian."
+        ]
+    },
+    {
+        category: "Obese",
+        tips: [
+            "Berolahraga secara teratur dengan aktivitas aerobik dan kekuatan untuk membakar kalori dan membangun otot.",
+            "Kurangi konsumsi makanan yang kaya lemak dan gula.",
+            "Fokus pada makanan sehat yang kaya nutrisi dan rendah kalori seperti buah-buahan, sayuran, protein rendah lemak, dan biji-bijian."
+        ]
+    }
+];
