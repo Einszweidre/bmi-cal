ba18dbe07f631ddf1947be8250f0072805bc3378
Einszweidre <hangga95@gmail.com>
2023-04-06 16:38:51 +0700

    Hangga

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ function calculateBMI() {
     let height = parseFloat(document.getElementById("height").value);
     let weight = parseFloat(document.getElementById("weight").value);
 
-    let bmi = weight / (height ** 2);
+    let bmi = weight / ((height/100) ** 2);
 
     document.getElementById("result").innerHTML = "Your BMI is: " + bmi.toFixed(2);
 
     if (bmi < 18.5) {
-        document.getElementById("category").innerHTML = "You are underweight.";
+        document.getElementById("category").innerHTML = `You are underweight. You need to increase your weight by ${(((height/100) ** 2)*18.5 - weight).toFixed(2)} kg`;
     } else if (bmi >= 18.5 && bmi < 25) {
-        document.getElementById("category").innerHTML = "You are of normal weight.";
+        document.getElementById("category").innerHTML = "You are of normal weight. ";
     } else if (bmi >= 25 && bmi < 30) {
-        document.getElementById("category").innerHTML = "You are overweight.";
+        document.getElementById("category").innerHTML = `You are overweight. You need to decrease your weight by ${(weight - ((height/100) ** 2)*24.9).toFixed(2)} kg`;
     } else {
-        document.getElementById("category").innerHTML = "You are obese.";
+        document.getElementById("category").innerHTML = `You are obese. You need to decrease your weight by ${(weight - ((height/100) ** 2)*24.9).toFixed(2)} kg. Consult to doctor if needed.`;
     }
 }
 
@@ -23,7 +23,7 @@ let recommendations = [
         tips: [
             "Kenaikan berat badan sehat dengan meningkatkan asupan kalori secara bertahap dan seimbang.",
             "Latihan kekuatan untuk membangun otot dan meningkatkan kekuatan tubuh.",
-            "Konsumsi makanan yang kaya nutrisi seperti biji-bijian, protein, lemak sehat dan buah-buahan dan sayuran yang memiliki kandungan nutrisi yang tinggi."
+            "Konsumsi makanan yang kaya nutrisi seperti biji-bijian, protein, lemak sehat dan buah-buahan dan sayuran yang memiliki kandungan nutrisi yang tinggi. Prioritaskan makanan tinggi protein seperti susu, telur dan daging"
         ]
     },
     {
@@ -45,7 +45,7 @@ let recommendations = [
     {
         category: "Obese",
         tips: [
-            "Berolahraga secara teratur dengan aktivitas aerobik dan kekuatan untuk membakar kalori dan membangun otot.",
+            "Berolahraga secara teratur dengan aktivitas aerobik dan kekuatan untuk membakar kalori dan membangun otot. Apabila BMI anda melebihi 40 disarankan untuk menurunkan berat badan anda 10% sebelum anda dapat memulai olahraga yang berat.",
             "Kurangi konsumsi makanan yang kaya lemak dan gula.",
             "Fokus pada makanan sehat yang kaya nutrisi dan rendah kalori seperti buah-buahan, sayuran, protein rendah lemak, dan biji-bijian."
         ]
